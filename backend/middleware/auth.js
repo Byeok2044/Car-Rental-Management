c3367fcb11c50812b7fe847cf746bad62343bb68
Rei0133 <reychee05@gmail.com>
2026-07-02 20:43:28 +0800

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import BlacklistedToken from '../models/BlacklistedToken.js';
+import BlacklistedToken from '../models/Blacklistedtoken.js';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_fallback_secret_key';
 
